6f3e96cb32131b5d481292e47656d21620bf91f6
Neko <469867876@qq.com>
2018-05-17 11:14:53 +0800

    配置scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const shell = require('shelljs')
+const sass = require('gulp-sass')
 
 const files = [
     'views/*',
@@ -12,9 +13,19 @@ gulp.task('views', () => {
     gulp.watch(files, ['copy'])
 })
 
+gulp.task('scss', () => {
+    gulp.watch('./views/assets/css/*.scss', ['scss-covert'])
+})
+
+gulp.task('scss-covert', () => {
+    gulp.src('./views/assets/css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./views/assets/css/'))
+})
+
 gulp.task('copy', () => {
     shell.rm('-rf', './server/views')
     shell.cp('-R', './views/', './server')
 })
 
-gulp.task('default', ['views'])
+gulp.task('default', ['scss', 'views'])
