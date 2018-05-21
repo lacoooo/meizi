7b4e0fd255c471f08575e6aa9b9109356dbac537
neko <469867876@qq.com>
2018-05-21 20:28:51 +0800

    1

--- controller/index.ts
+++ controller/index.ts
@@ -3,11 +3,14 @@ import * as path from 'path'
 
 export default {
     index(page, type, url) {
-        const dir = path.resolve(`${__dirname}/../../../../data/${type}/${page}`)
+        const dir = path.resolve(`${__dirname}/../../../data/${type}/${page}`)
 
-        let titlesPath = fs.readdirSync(dir)[0]
-        let titles = fs.readFileSync(`${dir}/${titlesPath}`, 'utf-8')
-        let imgs = JSON.parse(titles).pics
+        let imgs = []
+        if (fs.existsSync(dir)) {
+            let titlesPath = fs.readdirSync(dir)[0]
+            let titles = fs.readFileSync(`${dir}/${titlesPath}`, 'utf-8')
+            imgs = JSON.parse(titles).pics
+          }
 
         const info = {
             url: url,
--- router/index.ts
+++ router/index.ts
@@ -14,18 +14,30 @@ api
     .get('/gif/:page', async (ctx, next) => {
         const page = ctx.params.page
         const info = controller.index(page, 'gif_doc', 'gif')
+        if (info.data.length < 1) {
+            ctx.redirect('/');ctx.status = 301
+            return
+        }
         await ctx.render('index', info)
     })
 
     .get('/gif2/:page', async (ctx, next) => {
         const page = ctx.params.page
         const info = controller.index(page, 'gif2_doc', 'gif2')
+        if (info.data.length < 1) {
+            ctx.redirect('/');ctx.status = 301
+            return
+        }
         await ctx.render('index', info)
     })
 
     .get('/pic/:page', async (ctx, next) => {
         const page = ctx.params.page
         const info = controller.index(page, 'pic_doc', 'pic')
+        if (info.data.length < 1) {
+            ctx.redirect('/');ctx.status = 301
+            return
+        }
         await ctx.render('index', info)
     })
 
